d533c6ead98a1947c59224e0ccbce5d09e35b773
John <compulsivecoder@gmail.com>
2019-03-18 03:01:25 +0000

    Added packages

--- get-libs.sh
+++ get-libs.sh
@@ -11,6 +11,6 @@ sh install-package.sh Newtonsoft.Json 11.0.2
 sh install-package.sh ArduinoSerialControllerClient 1.1.1.5
 sh install-package.sh duinocom.core 1.0.6
 sh install-package.sh M2Mqtt 4.3.0.0
-sh install-package.sh ArduinoPlugAndPlay 1.0.0.1
+sh install-package.sh ArduinoPlugAndPlay 1.0.0.2
 
 echo "Finished getting library files."
